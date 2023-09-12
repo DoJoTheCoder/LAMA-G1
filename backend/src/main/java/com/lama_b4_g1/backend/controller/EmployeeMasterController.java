37cccd00e55ea13f27a8c466cf3dcc877169299e
Gowtam Rao <89023072+GowtamRao716@users.noreply.github.com>
2023-09-12 15:55:15 +0530

    CORS update

--- backend/src/main/java/com/lama_b4_g1/backend/controller/EmployeeMasterController.java
+++ backend/src/main/java/com/lama_b4_g1/backend/controller/EmployeeMasterController.java
@@ -1,6 +1,9 @@
 package com.lama_b4_g1.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,7 @@ import com.lama_b4_g1.backend.models.EmployeeMaster;
 import com.lama_b4_g1.backend.services.EmployeeMasterService;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class EmployeeMasterController {
 
 	@Autowired
@@ -25,7 +29,8 @@ public class EmployeeMasterController {
 	public String loginEmployee(@RequestBody EmployeeLoginCredentials empLoginCred) {
 		if(empLoginCred==null) {
 			System.out.println("emp login cred is null!");
-			return "Null Credientials";
+			return "Null Credentials";
+//			return new ResponseEntity<>("Null Credientials", HttpStatus.OK);
 		}
 		String result = empMasterService.authenticateEmployee(empLoginCred);
 		System.out.println("Employee controller info:");
@@ -33,6 +38,7 @@ public class EmployeeMasterController {
 		System.out.println("Password:"+empLoginCred.getPassword());
 		System.out.println("\nResult:"+result);
 		return result;
+//		return new ResponseEntity<>(result,HttpStatus.OK);
 		
 	}
 }
